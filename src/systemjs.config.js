bedda4e1269492ea31a9d61a00c215bccc5afea0
pengkobe <yipeng.info@gmail.com>
2017-04-24 22:30:24 +0800

    add bootstrap3 modal

--- src/systemjs.config.js
+++ src/systemjs.config.js
@@ -22,7 +22,7 @@
       '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
       '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
       '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
-
+      'ng2-bs3-modal': 'npm:ng2-bs3-modal',
       // other libraries
       'rxjs':                      'npm:rxjs',
       'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js'
